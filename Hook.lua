6421198087f6ab77935c76216dcf28f13095fa9a
mrhackerman35297 <95538175+mrhackerman35297@users.noreply.github.com>
2023-12-20 22:51:11 -0500

    Update Hook.lua

--- Hook.lua
+++ Hook.lua
@@ -1,54 +1,200 @@
+getgenv().Enabled = true
+getgenv().Dis = function()
+	Enabled = false
 
+	for i,v in pairs(workspace:GetDescendants()) do 
+		if (v:IsA("Seat")) or (v:IsA("VehicleSeat")) then 
+			v.Disabled = true
+		end
+	end
+end
+
+if (not game:IsLoaded()) then 
+	game.Loaded:Wait()
+	task.wait(2)
+end
+
+repeat task.wait(0.2) until (game:GetService("Players").LocalPlayer) and (game:GetService("Players").LocalPlayer.Character)
+
+local Players = game:GetService("Players")
+local Cashiers = workspace.Cashiers
+local Player = Players.LocalPlayer
 local Profit = game.Players.LocalPlayer.DataFolder.Currency.Value
 local Tick = os.time()
 
 local money = function(Num)
 	return tostring(Num):reverse():gsub("%d%d%d", "%1,"):reverse():gsub("^,", "")
 end
-local url = "https://discord.com/api/webhooks/1137541903143948288/ePJDY-d2JIXqywNMIlyHTGuq4sDcC6_rnd6SNc04bw_FNg4kzfY5wqiwWAJq3-cxfKWF"
-    local data = {
-        ["content"] =  "",
-        ["embeds"] = {
-            {
-                ["title"] = ('Autofarm Log'),
-                ["description"] = "**Username:\n**"..game.Players.LocalPlayer.Name.." ("..game.Players.LocalPlayer.UserId..")",
-                ["type"] = "rich",
-                ["color"] = tonumber(0xFFFFFF),
-                ["fields"] = {
-                    {
-                        ["name"] = "Money:",
-                        ["value"] = '```'..money(game.Players.LocalPlayer.DataFolder.Currency.Value)..'```',
-                        ["inline"] = true,
-                    },
-                    {
-                        ["name"] = "Profits:",
-                        ["value"] = '```'..money(game.Players.LocalPlayer.DataFolder.Currency.Value - Profit)..'```',
-                        ["inline"] = true,
-                    },
-                    {
-                        ["name"] = "Bounty:",
-                        ["value"] = '```'..money(game.Players.LocalPlayer.leaderstats.Wanted.Value)..'```',
-                        ["inline"] = true,
-                    },
-                    {
-                        ["name"] = "Elapsed:",
-                        ["value"] = '```'..string.format("%02i:%02i:%02i", (os.time()-Tick)/60^2, (os.time()-Tick)/60%60, (os.time()-Tick)%60)..'```',
-                        ["inline"] = false,
-                    },
-                    {
-                        ["name"] = "Credits:",
-                        ["value"] = '9kDestroyer',
-                        ["inline"] = false,
-                    },
-                }, 
+loadstring(game:HttpGet("https://github.com/applless/RandomScripts/raw/main/AntiAfk"))()
+pcall(function()local a=game:GetService("ReplicatedStorage").MainEvent;local b={"CHECKER_1","TeleportDetect","OneMoreTime"}local c;c=hookmetamethod(game,"__namecall",function(...)local d={...}local self=d[1]local e=getnamecallmethod()local f=getcallingscript()if e=="FireServer"and self==a and table.find(b,d[2])then return end return c(...)end)end)
+
+for i,v in pairs(workspace:GetDescendants()) do 
+	if (v:IsA("Seat")) or (v:IsA("VehicleSeat")) then 
+		v.Disabled = true
+	end
+end
+
+
+
+local GetClosestPart = function(Table)
+	local ClosestPart = nil
+
+	for i,v in pairs(Table) do
+		if (ClosestPart == nil) then
+			ClosestPart = v
+		else
+			if ((Player.Character.HumanoidRootPart.Position - v.Position).Magnitude) < ((ClosestPart.Position - Player.Character.HumanoidRootPart.Position).Magnitude) then
+				ClosestPart = v
+			end
+		end
+	end
+	
+	return ClosestPart
+end
+
+local CheckKOed = function()
+	if (Player.Character.BodyEffects["K.O"].Value == true) then 
+		Player.Character.Humanoid.Health = 0 
+
+		for i,v in pairs(Player.Character:GetChildren()) do 
+			if (v:IsA("BasePart")) then 
+				v:Destroy()
+			end
+		end
+	end
+end
+
+local GetCashier = function()
+	local AvailableCashiers = {}
+	for i,v in pairs(Cashiers:GetChildren()) do 
+		if (v:FindFirstChild("Humanoid")) and (v.Humanoid.Health > 0) then 
+			AvailableCashiers[#AvailableCashiers+1] = v.Open
+		end
+	end
+	
+	local ClosestPart = GetClosestPart(AvailableCashiers)
+	
+	if (ClosestPart) and (ClosestPart.Parent) then 
+		return ClosestPart.Parent
+	else 
+		return nil
+	end
+
+end
+
+local GetCashParts = function()
+	local CashParts = {}
+	for i,v in pairs(workspace.Ignored.Drop:GetChildren()) do 
+		if (v.Name == "MoneyDrop") and ((Player.Character.HumanoidRootPart.Position - v.Position).Magnitude < 13) then 
+			CashParts[#CashParts+1] = v
+		end
+	end
+	
+	return CashParts
+end
+
+
+task.spawn(function()
+	while true and task.wait(0.33) do 
+		if (Enabled == true) and (Player.Character) and (Player.Character:FindFirstChild("FULLY_LOADED_CHAR"))  then 
+			local Cashier = nil
+			repeat 
+				Cashier = GetCashier()
+				task.wait()
+			until (Cashier ~= nil)
+
+			
+			repeat 
+				if (Player.Backpack:FindFirstChild("Combat")) then 
+					task.wait(0.77)
+					pcall(function()
+						Player.Backpack.Combat.Parent = Player.Character
+					end)
+					task.wait()
+				end
+				Player.Character.HumanoidRootPart.Velocity = Vector3.new(0, 0, 0)
+				Player.Character.HumanoidRootPart.CFrame = Cashier.Open.CFrame * CFrame.Angles(0, math.random(-180, 0), 0)
+				
+				if (Player.Character:FindFirstChild("Combat")) then 
+					Player.Character.Combat:Activate()
+					--Player.Character.Combat:Deactivate() Disabled because super punch is faster and more reliable on low fps.
+				end
+
+				
+				task.wait()
+			until (Cashier.Humanoid.Health <= 0) or (Player.Character.Humanoid.Health <= 0) or (Player.Character.BodyEffects["K.O"].Value == true) or (Enabled == false)
+			
+			pcall(CheckKOed)
+			
+			repeat 
+				local CashParts = GetCashParts()
+				
+				Player.Character.HumanoidRootPart.Velocity = Vector3.new(0, 0, 0)
+				Player.Character.HumanoidRootPart.CFrame = Cashier.Open.CFrame + Vector3.new(0, 2, 0)
+				
+				for i,v in pairs(CashParts) do 
+					if (v:FindFirstChild("ClickDetector")) then 
+						fireclickdetector(v.ClickDetector)
+					end
+				end
+
+				task.wait()
+			until (#CashParts <= 0) or (Player.Character.Humanoid.Health <= 0) or (Player.Character.BodyEffects["K.O"].Value == true) or (Enabled == false)
+			
+			pcall(CheckKOed)
+		end
+	end
+end)
+
+
+task.spawn(function()
+    while wait(600) do
+        local url = _G.Webhook
+        local data = {
+            ["content"] =  "",
+            ["embeds"] = {
+                {
+                    ["title"] = ('Autofarm Log'),
+                    ["description"] = "**Username:\n**"..game.Players.LocalPlayer.Name.." ("..game.Players.LocalPlayer.UserId..")",
+                    ["type"] = "rich",
+                    ["color"] = tonumber(0xFFFFFF),
+                    ["fields"] = {
+                        {
+                            ["name"] = "Money:",
+                            ["value"] = '```'..money(game.Players.LocalPlayer.DataFolder.Currency.Value)..'```',
+                            ["inline"] = true,
+                        },
+                        {
+                            ["name"] = "Profits:",
+                            ["value"] = '```'..money(game.Players.LocalPlayer.DataFolder.Currency.Value - Profit)..'```',
+                            ["inline"] = true,
+                        },
+                        {
+                            ["name"] = "Bounty:",
+                            ["value"] = '```'..money(game.Players.LocalPlayer.leaderstats.Wanted.Value)..'```',
+                            ["inline"] = true,
+                        },
+                        {
+                            ["name"] = "Elapsed:",
+                            ["value"] = '```'..string.format("%02i:%02i:%02i", (os.time()-Tick)/60^2, (os.time()-Tick)/60%60, (os.time()-Tick)%60)..'```',
+                            ["inline"] = false,
+                        },
+                        {
+                            ["name"] = "Credits:",
+                            ["value"] = '@9kDestroyer',
+                            ["inline"] = false,
+                        },
+                    }, 
+                }
             }
         }
-    }
-    local newdata = game:GetService("HttpService"):JSONEncode(data)
-    
-    local headers = {
-        ["content-type"] = "application/json"
-    }
-    request = http_request or request or HttpPost or syn.request
-    local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
-    request(abcdef)
+        local newdata = game:GetService("HttpService"):JSONEncode(data)
+        
+        local headers = {
+            ["content-type"] = "application/json"
+        }
+        request = http_request or request or HttpPost or syn.request
+        local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
+        request(abcdef)
+    end
+end)
